a15f68b2c5627ff81e28136c65b02456062152c2
Rizky Dhani <rizkydhani15@gmail.com>
2024-12-09 11:49:38 +0700

    added dropdown css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 
     <title>@yield('title') - {{ env('APP_NAME') }}</title>
 
-    <link href="{{ asset('assets/css/adminkit-pro.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/adminkit.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
 </head>
 

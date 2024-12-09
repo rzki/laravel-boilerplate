14633ecf12afe9ce51d715041c456f8c5cc28277
Rizky Dhani <rizkydhani15@gmail.com>
2024-12-09 11:41:25 +0700

    change css from tailwind to bootstrap, added adminkit template as admin template

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('title', 'Dashboard')
+@section('content')
+    <div class="p-0 container-fluid">
+        <h1 class="mb-3 h3"><strong>Dashboard</strong></h1>
+        <div class="row">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="mt-0 col">
+                            <h5 class="card-title"></h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
